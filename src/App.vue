0af98072e38c474e651eb5011f98d24543b2763e
hashrock <hashrock@users.noreply.github.com>
2024-09-22 14:36:48 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import { useGameEngine } from "./useGameEngine";
 import LogPane from "./components/LogPane.vue";
 import titleImage from "./assets/event/title.jpg";
 import introImage from "./assets/event/intro.png";
+import roadImage from "./assets/bg/road.png";
+
 import Button from "./components/Button.vue";
 import { onClickOutside } from "@vueuse/core";
 import { PropmtItem } from "./types";
@@ -60,13 +62,82 @@ function* storyIntro() {
   if (okng === "y") {
     addLog("コン「いいよ」");
     yield* waitKey();
-    addLog("おばあちゃん「ありがとう、これを持っておゆき」");
+    addLog("おばあちゃん「ありがとう、気を付けて行ってきてね」");
+    yield* waitKey();
+    iter.value = storyMountain();
   } else {
     addLog("コン「嫌です」");
     yield* wait(20);
     addLog("おばあちゃん「ええ〜」");
     yield* wait(50);
     addLog("GAME OVER");
+  }
+}
+
+let fieldLength = 100;
+let fieldPosition = ref(0);
+
+function randomBool(threshold: number) {
+  return Math.random() < threshold;
+}
+
+function randomInt(max: number) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function* storyMountain() {
+  imageSrc.value = roadImage;
+
+  addLog("コン「裏山に到着した」");
+  yield* waitKey();
+  addLog("コン「薬草を探すか」");
+
+  while (true) {
+    const okng = yield* prompt([
+      { label: "登る", value: "next" },
+      { label: "戻る", value: "prev" },
+    ]);
+
+    if (okng === "next") {
+      fieldPosition.value += 1;
+      addLog("コンは山道を進んだ");
+      yield* wait(10);
+
+      addLog("コン「ふう、ふう」");
+      if (randomBool(0.3)) {
+        addLog("コン「茂みになにかありそう」");
+        const selection = yield* prompt([
+          { label: "探す", value: "search" },
+          { label: "通り過ぎる", value: "pass" },
+        ]);
+        if (selection === "search") {
+          switch (randomInt(2)) {
+            case 0:
+              addLog("コン「ありそうで何もなかった」");
+              break;
+            case 1:
+              addLog("コン「なんもないな〜」");
+              break;
+            case 2:
+              addLog("かっこいい枝を見つけた");
+              break;
+          }
+        }
+      }
+
+      if (fieldPosition.value >= fieldLength) {
+        addLog("コン「薬草を見つけた」");
+        yield* waitKey();
+        break;
+      }
+    } else {
+      fieldPosition.value -= 1;
+      if (fieldPosition.value <= 0) {
+        addLog("コン「裏山を下りた」");
+        yield* waitKey();
+        break;
+      }
+    }
   }
 }
 
@@ -87,9 +158,15 @@ function onClickPrompt(item: PropmtItem) {
 </script>
 
 <template>
-  <div class="w-[600px] h-[800px] bg-white rounded-xl p-4 flex flex-col">
+  <div
+    class="w-[600px] h-[800px] bg-white rounded-xl p-4 flex flex-col select-none"
+  >
     <div class="w-[600px] h-[550px] flex items-center overflow-clip">
-      <img :src="imageSrc" alt="image" class="w-full object-contain" />
+      <img
+        :src="imageSrc"
+        alt="image"
+        class="w-full object-contain pointer-events-none"
+      />
     </div>
     <div class="flex flex-col">
       <div class="flex gap-4">
